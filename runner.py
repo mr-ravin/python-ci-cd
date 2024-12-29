cdc071dd5da902626c693ff086270ded0e1d8c2b
Ravin Kumar <16964978+mr-ravin@users.noreply.github.com>
2024-12-29 14:44:14 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -10,7 +10,5 @@ if mode == "production":
     print("production code was running.")
 elif mode == "test":
     print("testing code was running.")
-    print("failure test")
-    raise Exception("Script threw error")
 else:
     print("some other code was running.")
